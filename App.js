c0ec8c8e81dd08b5cb47b481d2278286246e5ff4
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-07-15 16:43:21 +0200

    Show all games

--- App.js
+++ App.js
@@ -1,40 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Button,
-  TouchableHighlight,
-} from "react-native";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { Main } from "./components/Main";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <Image
-        source={{
-          uri: "https://upload.wikimedia.org/wikipedia/en/0/02/My_Neighbor_Totoro_-_Tonari_no_Totoro_%28Movie_Poster%29.jpg",
-        }}
-        style={{ width: 215, height: 294 }}
-      />
-      <Text style={{ color: "white" }}>Metacritic App!</Text>
-      <Button title="Press to enter" onPress={() => alert("Hi!")} />
-      <TouchableHighlight
-        underlayColor={"#09f"}
-        onPress={() => alert("Hi!")}
-        style={{
-          width: 200,
-          height: 200,
-          backgroundColor: "red",
-          borderRadius: 100,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text style={{ color: "white" }}>Press here!</Text>
-      </TouchableHighlight>
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -44,5 +21,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
+    paddingHorizontal: 12,
   },
 });
