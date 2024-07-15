8a60cc3923961186113f5f95cc3b68630ee4213e
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-07-15 15:33:26 +0200

    Activate eslint and prettier

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableHighlight,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri: "https://upload.wikimedia.org/wikipedia/en/0/02/My_Neighbor_Totoro_-_Tonari_no_Totoro_%28Movie_Poster%29.jpg",
+        }}
+        style={{ width: 215, height: 294 }}
+      />
+      <Text style={{ color: "white" }}>Metacritic App!</Text>
+      <Button title="Press to enter" onPress={() => alert("Hi!")} />
+      <TouchableHighlight
+        underlayColor={"#09f"}
+        onPress={() => alert("Hi!")}
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: "red",
+          borderRadius: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "white" }}>Press here!</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -13,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#09f',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
